3faf4baa71cf501b4aae1885dda0a44c7318084f
Muhammad Rafi <murafi@cisco.com>
2022-11-07 19:25:36 +0000

    added student-data.json

--- xml_to_json.py
+++ xml_to_json.py
@@ -6,7 +6,7 @@ import json
 import sys
 
 # pass the xml file location with the script 
-# e.g "./xml_to_json.py ./student-data/student-data.xml"
+# e.g " ./xml_to_json.py ./student-data/student-data.xml | tee ./student-data/student-data.json"
 with open(sys.argv[1], "r") as f:
    obj = xmltodict.parse(f.read())
 
